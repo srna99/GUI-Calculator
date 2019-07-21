a627a04c763a8f22a39b6d9e065476b6acd696c7
Serena Cheng <serena.ch1999@gmail.com>
2019-07-21 16:42:31 -0400

    Try to implement decimal.

--- Calculator.py
+++ Calculator.py
@@ -2,14 +2,14 @@ import tkinter
 import math
 
 
-class Calculator:
+class Calculator():
     expression = ""
-    isSolved = False
+    is_solved = True
 
-    def __init__(self, window):
+    def __init__(self, window: tkinter.Tk):
 
         window.title("Calculator")
-        window.geometry("275x380")
+        window.geometry("275x440")
         window.configure(bg="#e7e6e1", padx=5, pady=10)
 
         self.display = tkinter.StringVar(window, "0")
@@ -21,107 +21,158 @@ class Calculator:
 
         button1 = tkinter.Button(window, text="1", height=3, width=6, activebackground="#e8e5d1",
                                  bg="#f9f6e2", fg="#222222", command=lambda: self.number_pressed("1"))
-        button1.grid(row=5, column=0)
+        button1.grid(row=6, column=0, pady=5)
 
         button2 = tkinter.Button(window, text="2", height=3, width=6, activebackground="#e8e5d1",
                                  bg="#f9f6e2", fg="#222222", command=lambda: self.number_pressed("2"))
-        button2.grid(row=5, column=1)
+        button2.grid(row=6, column=1)
 
         button3 = tkinter.Button(window, text="3", height=3, width=6, activebackground="#e8e5d1",
                                  bg="#f9f6e2", fg="#222222", command=lambda: self.number_pressed("3"))
-        button3.grid(row=5, column=2)
+        button3.grid(row=6, column=2)
 
         button4 = tkinter.Button(window, text="4", height=3, width=6, activebackground="#e8e5d1",
                                  bg="#f9f6e2", fg="#222222", command=lambda: self.number_pressed("4"))
-        button4.grid(row=4, column=0, pady=5)
+        button4.grid(row=5, column=0)
 
         button5 = tkinter.Button(window, text="5", height=3, width=6, activebackground="#e8e5d1",
                                  bg="#f9f6e2", fg="#222222", command=lambda: self.number_pressed("5"))
-        button5.grid(row=4, column=1)
+        button5.grid(row=5, column=1)
 
         button6 = tkinter.Button(window, text="6", height=3, width=6, activebackground="#e8e5d1",
                                  bg="#f9f6e2", fg="#222222", command=lambda: self.number_pressed("6"))
-        button6.grid(row=4, column=2)
+        button6.grid(row=5, column=2)
 
         button7 = tkinter.Button(window, text="7", height=3, width=6, activebackground="#e8e5d1",
                                  bg="#f9f6e2", fg="#222222", command=lambda: self.number_pressed("7"))
-        button7.grid(row=3, column=0)
+        button7.grid(row=4, column=0, pady=5)
 
         button8 = tkinter.Button(window, text="8", height=3, width=6, activebackground="#e8e5d1",
                                  bg="#f9f6e2", fg="#222222", command=lambda: self.number_pressed("8"))
-        button8.grid(row=3, column=1)
+        button8.grid(row=4, column=1)
 
         button9 = tkinter.Button(window, text="9", height=3, width=6, activebackground="#e8e5d1",
                                  bg="#f9f6e2", fg="#222222", command=lambda: self.number_pressed("9"))
-        button9.grid(row=3, column=2)
+        button9.grid(row=4, column=2)
 
         button0 = tkinter.Button(window, text="0", height=3, width=6, activebackground="#e8e5d1",
                                  bg="#f9f6e2", fg="#222222", command=lambda: self.number_pressed("0"))
-        button0.grid(row=6, column=1, pady=5)
+        button0.grid(row=7, column=0)
+
+        decimal_button = tkinter.Button(window, text=".", height=3, width=6, activebackground="#e8e5d1",
+                                        bg="#f9f6e2", fg="#222222",
+                                        command=lambda: self.number_pressed("."))
+        decimal_button.grid(row=7, column=1)
+
+        sign_button = tkinter.Button(window, text="±", height=3, width=6, activebackground="#e8e5d1",
+                                     bg="#f9f6e2", fg="#222222",
+                                     command=lambda: self.function_pressed("±"))
+        sign_button.grid(row=7, column=2)
 
         divide_button = tkinter.Button(window, text="÷", height=3, width=6, activebackground="#446a84",
                                        activeforeground="#eeeeee", bg="#557b95", fg="#eeeeee",
                                        command=lambda: self.function_pressed("/"))
-        divide_button.grid(row=2, column=3)
+        divide_button.grid(row=3, column=3)
 
         multiply_button = tkinter.Button(window, text="×", height=3, width=6, activebackground="#446a84",
                                          activeforeground="#eeeeee", bg="#557b95", fg="#eeeeee",
                                          command=lambda: self.function_pressed("*"))
-        multiply_button.grid(row=3, column=3)
+        multiply_button.grid(row=4, column=3)
 
         subtract_button = tkinter.Button(window, text="-", height=3, width=6, activebackground="#446a84",
                                          activeforeground="#eeeeee", bg="#557b95", fg="#eeeeee",
                                          command=lambda: self.function_pressed("-"))
-        subtract_button.grid(row=4, column=3)
+        subtract_button.grid(row=5, column=3)
 
         add_button = tkinter.Button(window, text="+", height=3, width=6, activebackground="#446a84",
                                     activeforeground="#eeeeee", bg="#557b95", fg="#eeeeee",
                                     command=lambda: self.function_pressed("+"))
-        add_button.grid(row=5, column=3)
+        add_button.grid(row=6, column=3)
 
         equal_button = tkinter.Button(window, text="=", height=3, width=6, activebackground="#446a84",
                                       activeforeground="#eeeeee", bg="#557b95", fg="#eeeeee",
                                       command=self.solve)
-        equal_button.grid(row=6, column=3)
+        equal_button.grid(row=7, column=3)
 
         clear_button = tkinter.Button(window, text="AC", height=3, width=6, activebackground="#aaaba8",
                                       bg="#c1c0b9", fg="#222222", command=self.clear)
-        clear_button.grid(row=6, column=0)
-
-        sign_button = tkinter.Button(window, text="±", height=3, width=6, activebackground="#aaaba8",
-                                     bg="#c1c0b9", fg="#222222",
-                                     command=lambda: self.function_pressed("±"))
-        sign_button.grid(row=6, column=2)
-
-        square_button = tkinter.Button(window, text="x²", height=3, width=6, activebackground="#aaaba8",
-                                       bg="#c1c0b9", fg="#222222",
-                                       command=lambda: self.function_pressed("x²"))
-        square_button.grid(row=2, column=0, pady=5)
-
-        sqrt_button = tkinter.Button(window, text="√", height=3, width=6, activebackground="#aaaba8",
-                                     bg="#c1c0b9", fg="#222222",
-                                     command=lambda: self.function_pressed("√"))
-        sqrt_button.grid(row=2, column=1)
+        clear_button.grid(row=2, column=3)
 
         percent_button = tkinter.Button(window, text="%", height=3, width=6, activebackground="#aaaba8",
                                         bg="#c1c0b9", fg="#222222",
                                         command=lambda: self.function_pressed("%"))
-        percent_button.grid(row=2, column=2)
+        percent_button.grid(row=3, column=0)
+
+        square_button = tkinter.Button(window, text="x²", height=3, width=6, activebackground="#aaaba8",
+                                       bg="#c1c0b9", fg="#222222",
+                                       command=lambda: self.function_pressed("x²"))
+        square_button.grid(row=3, column=1)
+
+        sqrt_button = tkinter.Button(window, text="√", height=3, width=6, activebackground="#aaaba8",
+                                     bg="#c1c0b9", fg="#222222",
+                                     command=lambda: self.function_pressed("√"))
+        sqrt_button.grid(row=3, column=2)
+
+        log_button = tkinter.Button(window, text="log", height=3, width=6, activebackground="#aaaba8",
+                                    bg="#c1c0b9", fg="#222222",
+                                    command=lambda: self.function_pressed("log"))
+        log_button.grid(row=2, column=0, pady=5)
+
+        factorial_button = tkinter.Button(window, text="x!", height=3, width=6,
+                                          activebackground="#aaaba8", bg="#c1c0b9", fg="#222222",
+                                          command=lambda: self.function_pressed("!"))
+        factorial_button.grid(row=2, column=1)
+
+        fraction_button = tkinter.Button(window, text="⅟ x", height=3, width=6,
+                                         activebackground="#aaaba8",
+                                         bg="#c1c0b9", fg="#222222",
+                                         command=lambda: self.function_pressed("1/x"))
+        fraction_button.grid(row=2, column=2)
 
     def number_pressed(self, number):
 
-        if not self.isSolved:
-            self.expression += number
-        else:
+        # if number == ".":
+        #     pos_expression =
+        #
+        #     if self.expression.isdigit():
+        #         return
+        #     elif self.is_solved:
+        #         self.expression = "0."
+        #         self.is_solved = False
+        #     else:
+        #         if not self.expression[-1].isdigit():
+        #             number = "0."
+        #         self.expression += number
+        # else:
+        #     if self.is_solved:
+        #         self.expression = number
+        #         self.is_solved = False
+        #     else:
+        #         self.expression += number
+
+        if self.is_solved:
+            if number == ".":
+                number = "0."
+
             self.expression = number
-            self.isSolved = False
+            self.is_solved = False
+        else:
+            if number == ".":
+                is_int = math.floor(float(self.expression)) == int(self.expression)
+
+                if not is_int:
+                    return
+                elif not self.expression[-1].isdigit():
+                    number = "0."
+
+            self.expression += number
 
         self.display.set(self.expression)
 
     def function_pressed(self, symbol):
 
         if self.expression != "":
-            self.isSolved = False
+            self.is_solved = False
             if symbol == "+" or symbol == "-" or symbol == "*" or symbol == "/":
                 self.expression += symbol
                 self.display.set(self.expression)
@@ -131,7 +182,7 @@ class Calculator:
                 elif symbol == "x²":
                     try:
                         self.expression = str(round(pow(float(self.expression), 2), 7))
-                    except OverflowError as e:
+                    except OverflowError:
                         self.display.set("error")
                         self.expression = ""
                 elif symbol == "√":
@@ -146,7 +197,7 @@ class Calculator:
         try:
             self.display.set(str(eval(self.expression)))
             self.expression = self.display.get()
-            self.isSolved = True
+            self.is_solved = True
         except (SyntaxError, ZeroDivisionError, ValueError, OverflowError) as e:
             print("ERROR: ", e)
             self.display.set("error")
